059e3d683dc3b6000622a12a0be29beab3e8fb11
BuildTools <unconfigured@null.spigotmc.org>
2022-11-28 20:53:08 -0600

    restored add song functionality

--- src/app/components/library.component.ts
+++ src/app/components/library.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   <div class="library container mt-5">
     <ul class="list-group">
       <li *ngFor="let song of songs" class="list-group-item" routerLink="{{song.id}}">{{song.title}}</li>
-      <!-- <li class="list-group-item lib-song" (click)="newSong()">new song +</li> -->
+      <li class="list-group-item lib-song" (click)="newSong()">new song +</li>
     </ul>
   </div>
   `,
